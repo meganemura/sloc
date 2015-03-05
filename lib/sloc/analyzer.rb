ba129c8cc58ae5a267bae38f823b503fb37380b2
meganemura <meganemura@users.noreply.github.com>
2015-03-06 02:31:17 +0900

    Implement Analyzer#single_comment_expression

--- lib/sloc/analyzer.rb
+++ lib/sloc/analyzer.rb
@@ -15,5 +15,13 @@ module Sloc
 
       result
     end
+
+    def single_comment_expression(extension)
+      case extension
+      when ".rb" then /#/
+      when ".vim" then /"/
+      else nil
+      end
+    end
   end
 end
