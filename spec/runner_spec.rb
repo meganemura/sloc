36cb09b7315aa6765477d110dbe4bf0bd6b0c949
meganemura <meganemura@users.noreply.github.com>
2015-03-14 12:53:58 +0900

    Add spec about Runner#report order

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -19,5 +19,29 @@ describe Sloc::Runner do
         expect(instance.report(files)).to be_a Hash
       end
     end
+
+    context 'with order options' do
+      let(:options) do
+        { order: Sloc::Analyzer::REPORT_KEYS.sample }
+      end
+
+      let(:report) do
+        instance.report(files)
+      end
+
+      let(:order) do
+        options[:order]
+      end
+
+      it 'returns hash' do
+        expect(report).to be_a Hash
+      end
+
+      it 'returns hash and it is ordered' do
+        report.each_cons(2) do |(_ak, av), (_bk, bv)|
+          expect(av[order] <= bv[order]).to be true
+        end
+      end
+    end
   end
 end
